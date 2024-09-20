7666f440cedf74ceeffe972f94dbc762761b1591
Rubberduckduck <bryanlim805@gmail.com>
2024-09-21 00:27:45 +0800

    Update MenuState.cpp

--- src/states/MenuState.cpp
+++ src/states/MenuState.cpp
@@ -36,17 +36,17 @@ void MenuState::init() {
 	// NIKEEngine.accessSystem("audio")->getActiveState();
 	NE_AUDIO_GROUP test_group = AudioSystem::AudioSystem::getInstance().CreateAudioGroup("group 1");
 	NE_AUDIO test_sound = AudioSystem::AudioSystem::getInstance().NEAudioLoadSound("assets/Audio/test_sound.ogg", "test_sound");
-	AudioSystem::AudioSystem::getInstance().NEAudioLoadMusic("assets/Audio/test_music.wav", "test_music");
-	// Audio system testing (1st way of playing audio)
 	AudioSystem::AudioSystem::getInstance().NEAudioPlay(test_sound, test_group, 1.f, 1.f, 0);
 
 	// 2nd way of playing audio
 	AudioSystem::AudioSystem::getInstance().CreateAudioGroup("group 2");
+	AudioSystem::AudioSystem::getInstance().NEAudioLoadMusic("assets/Audio/test_music.wav", "test_music");
 	AudioSystem::AudioSystem::getInstance().NEAudioPlay("test_music", "group 2", 1.f, 1.f, 0);
 
 }
 
 void MenuState::update() {
+	// Press Q to pause song
 	AudioSystem::AudioSystem::getInstance().NEAudioStopGroup("group 2");
 }
 
