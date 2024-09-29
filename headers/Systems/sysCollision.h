364b131babad1d58d17773f4a9551e71197e1f2a
ko.m <ko.m@digipen.edu>
2024-09-29 23:53:09 +0800

    Update for merge

--- headers/Systems/sysCollision.h
+++ headers/Systems/sysCollision.h
@@ -1,46 +1,36 @@
 /*****************************************************************//**
- * \file   CollisionManager.h
- * \brief  Basic collision system
+ * \file   sysCollision.h
+ * \brief  Collision manager header for the physics system.
  *
  * \author Min Khant Ko
  * \date   September 2024
  *********************************************************************/
 
-
 #pragma once
 
-#ifndef COLLISION_SYSTEM_HPP
-#define COLLISION_SYSTEM_HPP
+#ifndef SYS_COLLISION_HPP
+#define SYS_COLLISION_HPP
 
 #include "../headers/Core/stdafx.h"
-#include "../headers/Core/Engine.h"
-#include "../headers/Math/Vector2.h"
-#include "../headers/Systems/CollisionUtils.h"
+#include "../headers/Managers/mSystem.h"
 
-/*
 namespace Collision {
 
-    class Manager : public System::ISystem {
+    class Manager {
     private:
-        // Private constructor for singleton pattern
-        Manager() = default;
-
         // Delete Copy Constructor & Copy Assignment
         Manager(Manager const& copy) = delete;
         void operator=(Manager const& copy) = delete;
 
         struct Collider {
-            // Note Vector2 default initialized to zero vector already
-
             bool active = false; // Tracks if collider is in use
-
             int objType = 7; // Placeholder for object type
 
-            Vector2 position;  // Position of the object (X, Y) world coordinates...
+            Vector2 position;  // Position of the object (X, Y) world coordinates
             Vector2 size;      // Size (width, height)
             float radius = 0.0f; // Optional for circle colliders
             Vector2 velocity; // Current velocity
-            
+
             // For bounding box
             Vector2 rect_min;
             Vector2 rect_max;
@@ -50,32 +40,49 @@ namespace Collision {
             Vector2 cir_max;
         };
 
-        std::vector<Collider> colliders;  // List of colliders in the system (CURRENTLY STORES ALL COLLIDERS)
+        std::vector<Collider> colliders;  // List of colliders in the system
 
-        // Function to set bounding box for collider
-        void setBoundingBox(Collider);
+        // Private constructor for singleton pattern
+        Manager() = default;
 
-        // Function to detect collisions between two colliders!
-        bool detectCollision(const Collider& a, const Collider& b) const;
+        // Utility functions
+        template <typename T>
+        T getMax(const T a, const T b);
 
-        // Test function to check for collision between 2 positions...
-        bool checkCollisionBetween( const Vector2& vel_a, const Vector2& rect_min_a, const Vector2& rect_max_a,
-                                    const Vector2& vel_b, const Vector2& rect_min_b, const Vector2& rect_max_b,
-                                    float radius_a, float radius_b);
+        template <typename T>
+        T getMin(const T a, const T b);
 
+        /* Raw collision functions */
+
+        // Circle collision check
+        bool circleCollisionCheck(const Vector2& pos_a, float radius_a, const Vector2& pos_b, float radius_b);
+
+        // AABB collision check for rectangle to rectangle
+        bool aabbRectRectCheck(const Vector2& vel_a, const Vector2& vel_b,
+            const Vector2& min_a, const Vector2& max_a,
+            const Vector2& min_b, const Vector2& max_b,
+            float& firstTimeOfCollision);
+
+        // Separating Axis Theorem (SAT) check
+        bool satCheck(const Vector2& pos_a, const Vector2& size_a, const Vector2& pos_b, const Vector2& size_b);
+
+        // Function to detect if the mouse is inside a rectangle area
+        bool detectMClickRect(const Vector2& center, float width, float height);
+
+        // Function to detect if the mouse is inside a circle area
+        bool detectMClickCircle(const Vector2& center, float radius);
 
     public:
-        //Singleton Of Manager Class
-        static std::shared_ptr<Manager> getInstance() {
-            static std::shared_ptr<Manager> instance{ std::make_shared<Manager>() };
+        static Manager& getInstance() {
+            static Manager instance;
             return instance;
         }
 
         // Initialization
-        void init() override;
+        void init();
 
-        // Update - where collisions will be checked every frame
-        void update() override;
+        // Update - check for collisions between all active colliders
+        void update();
 
         // Register a new collider
         void registerCollider(const Vector2& position, const Vector2& size, float radius, const Vector2& velocity);
@@ -83,11 +90,12 @@ namespace Collision {
         // Unregister a collider
         void unregisterCollider(const Vector2& position);
 
+        // Detect ANY collision between 2 colliders
+        bool detectCollision(const Collider& a, const Collider& b) const;
+
         // Destructor
-        ~Manager() override = default;
+        ~Manager() = default;
     };
 }
 
-*/
-
-#endif // COLLISION_SYSTEM_HPP
+#endif // SYS_COLLISION_HPP
--- src/Systems/sysCollision.cpp
+++ src/Systems/sysCollision.cpp
@@ -1,107 +1,242 @@
 /*****************************************************************//**
- * \file   CollisionSystem.cpp
- * \brief  Basic collision system
+ * \file   sysCollision.cpp
+ * \brief  Implements the collision manager.
  *
  * \author Min Khant Ko
  * \date   September 2024
  *********************************************************************/
+
 #include "../headers/Core/stdafx.h"
 #include "../headers/Systems/sysCollision.h"
 
-/*
-void Collision::Manager::init() {
-    colliders.resize(300);  // Pre-allocate 300 colliders
-    for (auto& collider : colliders) {
-        collider.active = false;  // Mark all colliders as unused at start
-    }
-}
+namespace Collision {
 
-
-void Collision::Manager::update() { // Currently checks for collision between ALL used colliders
-    for (size_t i = 0; i < colliders.size(); ++i) {
-        if (!colliders[i].active) {
-            continue;  // Skip inactive colliders
+    void Manager::init() {
+        colliders.resize(100);  // Pre-allocate 100 colliders
+        for (auto& collider : colliders) {
+            collider.active = false;  // Mark all colliders as unused at start
         }
-        for (size_t j = i + 1; j < colliders.size(); ++j) {
-            if (!colliders[j].active) {
-                continue;  // Skip inactive colliders
-            }
-            // Apply detect collision between every collider
-            if (detectCollision(colliders[i], colliders[j])) {
-                cout << "Collision detected between objects " << i << " and " << j << endl;
+    }
+
+    void Manager::update() {
+        for (size_t i = 0; i < colliders.size(); ++i) {
+            if (!colliders[i].active) continue;
+
+            for (size_t j = i + 1; j < colliders.size(); ++j) {
+                if (!colliders[j].active) continue;
+
+                if (detectCollision(colliders[i], colliders[j])) {
+                    cout << "Collision detected between objects " << i << " and " << j << endl;
+                }
             }
         }
     }
-}
 
-
-bool Collision::Manager::detectCollision(const Collider& a, const Collider& b) const {
-    // Placeholder for collision time
-    float timeOfCollision = 0.0f;
-
-    // Check for Circle, then AABB, then SAT collision
-    if (a.radius > 0.0f && b.radius > 0.0f) {
-        return CollisionUtils::circleCollisionCheck(a.position, a.radius, b.position, b.radius);
+    void Manager::registerCollider(const Vector2& position, const Vector2& size, float radius, const Vector2& velocity) {
+        for (auto& collider : colliders) {
+            if (!collider.active) {
+                collider.active = true;
+                collider.position = position;
+                collider.size = size;
+                collider.radius = radius;
+                collider.velocity = velocity;
+                collider.rect_min = { position.x - (size.x / 2), position.y - (size.y / 2) };
+                collider.rect_max = { position.x + (size.x / 2), position.y + (size.y / 2) };
+                return;
+            }
+        }
+        cout << "Error: No available colliders to register!" << endl;
     }
-    else if (CollisionUtils::aabbRectRectCheck(a.velocity, b.velocity, a.rect_min, a.rect_max, b.rect_min, b.rect_max, timeOfCollision)) {
-        return true;
-    }
-    else {
-        return CollisionUtils::satCheck(a.position, a.size, b.position, b.size);
-    }
-}
 
-// Test function
-bool Collision::Manager::checkCollisionBetween( const Vector2& vel_a, const Vector2& rect_min_a, const Vector2& rect_max_a,
-                                                const Vector2& vel_b, const Vector2& rect_min_b, const Vector2& rect_max_b,
-                                                float radius_a, float radius_b) {
-    // Placeholder for collision time
-    float timeOfCollision = 0.0f;
-
-    // Check for Circle, then AABB, then SAT collision
-    if (radius_a > 0.0f && radius_b > 0.0f) {
-        return CollisionUtils::circleCollisionCheck(rect_min_a, radius_a, rect_min_b, radius_b);
+    void Manager::unregisterCollider(const Vector2& position) {
+        for (auto& collider : colliders) {
+            //if (collider.active && collider.position == position) {  // Find the collider based on position // Note Vector2 does not have defined/overloaded == or != operators currently
+            if (collider.active && (collider.position.x == position.x && collider.position.y == position.y)) {
+                collider.active = false;
+                return;
+            }
+        }
     }
-    else {
-        if (CollisionUtils::aabbRectRectCheck(vel_a, vel_b, rect_min_a, rect_max_a, rect_min_b, rect_max_b, timeOfCollision)) {
+
+    bool Manager::detectCollision(const Collider& a, const Collider& b) const {
+        // Placeholder for collision time
+        float timeOfCollision = 0.0f;
+
+        if (a.radius > 0.0f && b.radius > 0.0f) {
+            return CollisionUtils::circleCollisionCheck(a.position, a.radius, b.position, b.radius);
+        }
+        else if (CollisionUtils::aabbRectRectCheck(a.velocity, b.velocity, a.rect_min, a.rect_max, b.rect_min, b.rect_max, timeOfCollision)) {
             return true;
         }
         else {
-            return CollisionUtils::satCheck(rect_min_a, rect_max_a - rect_min_a, rect_min_b, rect_max_b - rect_min_b);
+            return CollisionUtils::satCheck(a.position, a.size, b.position, b.size);
         }
     }
-}
 
-
-// Register a collider...
-void Collision::Manager::registerCollider(const Vector2& position, const Vector2& size, float radius, const Vector2& velocity) {
-    for (auto& collider : colliders) {
-        if (!collider.active) {  // Find the first unused collider
-            collider.active = true;
-            collider.position = position;
-            collider.size = size;
-            collider.radius = radius;
-            collider.velocity = velocity;
-            collider.rect_min = { position.x - (size.x / 2), position.y - (size.y / 2) }; // Bounding box min
-            collider.rect_max = { position.x + (size.x / 2), position.y + (size.y / 2) }; // Bounding box max
-            return;  // Successfully registered
-        }
+    template <typename T>
+    T getMax(const T a, const T b) {
+        return (a > b) ? a : b;
     }
-    // If no available colliders, log an error
-    cout << "Error: No available colliders to register!" << endl;
-}
 
+    template <typename T>
+    T getMin(const T a, const T b) {
+        return (a < b) ? a : b;
+    }
 
-// Unregister a collider (Removes collider currently based on position...)
-void Collision::Manager::unregisterCollider(const Vector2& position) {
-    for (auto& collider : colliders) {
-        //if (collider.active && collider.position == position) {  // Find the collider based on position // Note Vector2 does not have defined/overloaded == or != operators currently
-        if (collider.active && (collider.position.x == position.x && collider.position.y == position.y)) {
-            collider.active = false;  // Mark the collider as unused
-            return;
+    bool circleCollisionCheck(const Vector2& pos_a, float radius_a, const Vector2& pos_b, float radius_b) {
+        float distX = pos_a.x - pos_b.x;
+        float distY = pos_a.y - pos_b.y;
+        float distance = std::sqrt(distX * distX + distY * distY);
+
+        return (distance < (radius_a + radius_b));
+    }
+
+    bool aabbRectRectCheck(const Vector2& vel_a, const Vector2& vel_b,
+        const Vector2& min_a, const Vector2& max_a,
+        const Vector2& min_b, const Vector2& max_b,
+        float& firstTimeOfCollision) {
+        // Step 1: Static collision detection
+        // Check if the two AABBs are overlapping statically
+        if (!(max_a.x < min_b.x || min_a.x > max_b.x ||
+            max_a.y < min_b.y || min_a.y > max_b.y)) {
+            // If there is an overlap in both x and y axes, a static collision occurs
+            return true;
         }
+
+        // Step 2: Calculate relative velocity for dynamic collision
+        Vector2 velRel = { vel_a.x - vel_b.x, vel_a.y - vel_b.y };
+
+        // Initialize time of first and last collision along each axis
+        Vector2 tFirst = { 0.0f, 0.0f };
+        Vector2 tLast = { NIKEEngine.getDeltaTime(), NIKEEngine.getDeltaTime() }; // Need a way to get time for previous frame
+
+        // Step 3: Check dynamic collision on x-axis
+        if (velRel.x != 0) {
+            if (velRel.x > 0) {
+                tFirst.x = (min_a.x - max_b.x) / velRel.x;
+                tLast.x = (max_a.x - min_b.x) / velRel.x;
+            }
+            else {
+                tFirst.x = (max_a.x - min_b.x) / velRel.x;
+                tLast.x = (min_a.x - max_b.x) / velRel.x;
+            }
+        }
+        else if (max_a.x < min_b.x || min_a.x > max_b.x) {
+            return false; // No collision on the x-axis if there's no relative movement and no static collision
+        }
+
+        // Step 4: Check dynamic collision on y-axis
+        if (velRel.y != 0) {
+            if (velRel.y > 0) {
+                tFirst.y = (min_a.y - max_b.y) / velRel.y;
+                tLast.y = (max_a.y - min_b.y) / velRel.y;
+            }
+            else {
+                tFirst.y = (max_a.y - min_b.y) / velRel.y;
+                tLast.y = (min_a.y - max_b.y) / velRel.y;
+            }
+        }
+        else if (max_a.y < min_b.y || min_a.y > max_b.y) {
+            return false; // No collision on the y-axis if there's no relative movement and no static collision
+        }
+
+        // Step 5: Check if collisions occur within the time frame
+        float tFirstOverall = getMax(tFirst.x, tFirst.y);
+        float tLastOverall = getMin(tLast.x, tLast.y);
+
+        // Ensure that the time of first collision occurs before the time of last collision
+        if (tFirstOverall > tLastOverall || tFirstOverall > NIKEEngine.getDeltaTime()) { // Need a way to get time for previous frame
+            return false; // No collision detected
+        }
+
+        // If we have a valid time of collision, update the output parameter
+        firstTimeOfCollision = tFirstOverall;
+
+        return true; // Collision detected
+    }
+
+
+
+    bool satCheck(const Vector2& pos_a, const Vector2& size_a, const Vector2& pos_b, const Vector2& size_b) {
+        // Calculate the half extents for each box
+        Vector2 half_extent_a(size_a.x / 2.0f, size_a.y / 2.0f);
+        Vector2 half_extent_b(size_b.x / 2.0f, size_b.y / 2.0f);
+
+        // Calculate the center points of both boxes
+        Vector2 center_a = pos_a + half_extent_a;
+        Vector2 center_b = pos_b + half_extent_b;
+
+        // Calculate the distance between the centers
+        Vector2 dist = center_b - center_a;
+
+        // Calculate overlap on the x-axis
+        float overlap_x = half_extent_a.x + half_extent_b.x - std::abs(dist.x);
+        if (overlap_x <= 0) return false;  // No overlap on x-axis, so no collision
+
+        // Calculate overlap on the y-axis
+        float overlap_y = half_extent_a.y + half_extent_b.y - std::abs(dist.y);
+        if (overlap_y <= 0) return false;  // No overlap on y-axis, so no collision
+
+        // If we get here, both axes overlap, so a collision exists
+        return true;
+    }
+
+    // Detect if the mouse is inside a rectangular area
+    bool detectMClickRect(const Vector2& center, float width, float height) {
+        // Get the mouse position from Input::Manager
+        float mouseX = Input::Manager::getInstance().mouse.x;
+        float mouseY = Input::Manager::getInstance().mouse.y;
+
+        // Calculate the boundaries of the rectangle
+        float left = center.x - (width / 2.0f);
+        float right = center.x + (width / 2.0f);
+        float top = center.y - (height / 2.0f);
+        float bottom = center.y + (height / 2.0f);
+
+        // Check if the mouse is inside the rectangle
+        if (mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom) {
+
+            // Check if the mouse is exactly at the center
+            if (mouseX == center.x && mouseY == center.y) {
+                cout << "Mouse is exactly at the center of the rectangle." << endl;
+            }
+            else {
+                // Print where the mouse is relative to the center of the rectangle
+                if (mouseX < center.x)
+                    cout << "Mouse is on the left side of the center." << endl;
+                else
+                    cout << "Mouse is on the right side of the center." << endl;
+
+                if (mouseY < center.y)
+                    cout << "Mouse is above the center." << endl;
+                else
+                    cout << "Mouse is below the center." << endl;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    // Detect if the mouse is inside a circular area
+    bool detectMClickCircle(const Vector2& center, float radius) {
+        // Get the mouse position from Input::Manager
+        float mouseX = Input::Manager::getInstance().mouse.x;
+        float mouseY = Input::Manager::getInstance().mouse.y;
+
+        // Calculate the distance from the mouse to the center of the circle
+        float distX = mouseX - center.x;
+        float distY = mouseY - center.y;
+        float distance = std::sqrt(distX * distX + distY * distY);
+
+        // Check if the mouse is inside the circle
+        if (distance <= radius) {
+            cout << "Mouse is inside the circle." << endl;
+            return true;
+        }
+
+        return false;
     }
 }
-
-
-*/
