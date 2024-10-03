7ba32af943904a2cd6de1b786d73ed6b1d53faec
hoshuhng <zendenho@gmail.com>
2024-10-03 09:23:46 +0800

    freetype DLL

--- src/Systems/Render/sysFont.cpp
+++ src/Systems/Render/sysFont.cpp
@@ -26,8 +26,8 @@ void Font::Manager::configQuadRendering() {
 	glGenBuffers(1, &VBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
 }
 
