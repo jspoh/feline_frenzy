4d4f5a6e4c412019e427e28f6075dcc6e221ab4f
Rubberduckduck <bryanlim805@gmail.com>
2024-11-06 00:43:18 +0800

    Loadded assets v1
    Asset file browser minor improvements
    
    For textures, able to show thumbnail of that texture

--- Nikesaurus/headers/Managers/Services/Imgui/ImguiUtils.h
+++ Nikesaurus/headers/Managers/Services/Imgui/ImguiUtils.h
@@ -15,16 +15,27 @@ namespace NIKE {
 
 	// This function will be used to update imgui's input if needed
 	void imguiInputUpdate();
-	// This function is used to check if the file has a valid extension (e.g .txt, .png)
+
+	// Checks if the file has a valid texture extension (e.g .png, .jpg).
 	bool hasValidTextureExtension(const std::filesystem::path& filePath);
-	// This function is used to check if the file has a valid extension (e.g .wav, .ogg)
+
+	// Checks if the file has a valid audio extension (e.g .wav, .ogg).
 	bool hasValidAudioExtension(const std::filesystem::path& filePath);
-	// This function is used to check if the file has a valid extension (e.g .txt, .scn)
+
+	// Checks if the file has a valid scene or text extension (e.g .txt, .scn).
 	bool hasValidScnTxtExtension(const std::filesystem::path& filePath);
-	// This function is used to check if the file has a valid extension (e.g .ttf)
+
+	// Checks if the file has a valid font extension (e.g .ttf).
 	bool hasValidFontExtension(const std::filesystem::path& filePath);
-	// This function is used to check if the file has a valid extension (e.g .vert)
+
+	// Checks if the file has a valid vertex shader extension (e.g .vert).
 	bool hasValidVertExtension(const std::filesystem::path& filePath);
+
+	// Checks if the file has a valid model extension (e.g .txt)
+	bool hasValidModelExtension(const std::filesystem::path& filePath);
+
+	// This function will be used to display assets of the given type
+	void displayAssetList(const std::string& assetType);
 
 }
 
