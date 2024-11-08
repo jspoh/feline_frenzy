a3078c95942c2262fc8805b8f7169176bbc7469d
jspoh <jingsengyeah@gmail.com>
2024-11-08 17:22:40 +0800

    unsure

--- Nikesaurus/src/Systems/Render/sysShader.cpp
+++ Nikesaurus/src/Systems/Render/sysShader.cpp
@@ -92,10 +92,11 @@ namespace NIKE {
 
 		int location = glGetUniformLocation(NIKE_ASSETS_SERVICE->getShader(shader_ref), name.c_str());
 		if (location >= 0) {
-			glUniform1iv(location, vals.size(), vals.data());
+			glUniform1iv(location, vals.size(), reinterpret_cast<const int*>(vals.data()));
 		}
 		else {
 			cerr << "Uniform location not found for: " << name << endl;
+			throw std::exception();
 		}
 
 		err = glGetError();
