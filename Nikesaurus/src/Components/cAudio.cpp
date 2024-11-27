afb3e7e0104b4603d080e2602a92cb559a77350d
Rubberduckduck <bryanlim805@gmail.com>
2024-11-28 00:24:13 +0800

    audio(SFX) comp UI
    audio_id(sfx) is chaned to combo selection

--- Nikesaurus/src/Components/cAudio.cpp
+++ Nikesaurus/src/Components/cAudio.cpp
@@ -44,50 +44,57 @@ namespace NIKE {
 			[](LevelEditor::ComponentsPanel& comp_panel, Audio::SFX& comp) {
 
 				//Static variables for audio string input management
-				static std::string audio_id;
 				static std::string channel_group_id;
 
 				//Initialization of string inputs upon collapsible shown
 				if (ImGui::IsItemActivated() || comp_panel.isEntityChanged()) {
-					audio_id = comp.audio_id;
 					channel_group_id = comp.channel_group_id;
 				}
 
 				//Set audio ID
 				{
-					//Channel id input
-					ImGui::Text("Enter Audio ID:");
-					if(ImGui::InputText("##AudioID", audio_id.data(), audio_id.capacity() + 1)) {
-						audio_id.resize(strlen(audio_id.c_str()));
+					// Display a combo box for selecting a audio
+					ImGui::Text("Select SFX:");
+
+					// Hold the current and previous audio selection
+					static std::string previous_audio_id = comp.audio_id;
+					std::string current_audio_id = comp.audio_id;
+
+					// Get all loaded audios
+					const auto& all_loaded_sounds = NIKE_ASSETS_SERVICE->getAssetRefs(Assets::Types::Sound);
+
+					// Find the index of the currently selected audio in the list
+					int current_index = -1;
+					for (size_t i = 0; i < all_loaded_sounds.size(); ++i) {
+						if (current_audio_id == all_loaded_sounds[i]) {
+							current_index = static_cast<int>(i);
+							break;
+						}
 					}
 
-					ImGui::SameLine();
+					// Display combo box for audio selection
+					if (ImGui::Combo("##SelectSFX", &current_index, all_loaded_sounds.data(), static_cast<int>(all_loaded_sounds.size()))) {
+						// Validate the selected index and get the new audio ID
+						if (current_index >= 0 && current_index < static_cast<int>(all_loaded_sounds.size())) {
+							std::string new_audio_id = all_loaded_sounds[current_index];
+							if (new_audio_id != comp.audio_id) {
+								// Save action
+								LevelEditor::Action change_audio_action;
+								change_audio_action.do_action = [&, audio_id = new_audio_id]() {
+									comp.audio_id = audio_id;
+									};
 
-					//Save audio ID Button
-					if (ImGui::Button("Save##AudioID")) {
-						if (NIKE_ASSETS_SERVICE->isAssetRegistered(audio_id)) {
-							LevelEditor::Action save_audio;
+								// Undo action
+								change_audio_action.undo_action = [&, audio_id = previous_audio_id]() {
+									comp.audio_id = audio_id;
+									};
 
-							//Save audio action
-							save_audio.do_action = [&, id = audio_id]() {
-								comp.audio_id = id;
-								audio_id = comp.audio_id;
-							};
+								// Execute the action
+								NIKE_LVLEDITOR_SERVICE->executeAction(std::move(change_audio_action));
 
-							//Undo save audio action
-							save_audio.undo_action = [&, id = comp.audio_id]() {
-								comp.audio_id = id;
-								audio_id = comp.audio_id;
-							};
-
-							NIKE_LVLEDITOR_SERVICE->executeAction(std::move(save_audio));
-							comp_panel.setPopUpSuccessMsg("Audio Saved!");
-							comp_panel.openPopUp("Success");
-						}
-						else {
-							comp_panel.setPopUpErrorMsg("Audio Does Not Exist!");
-							comp_panel.openPopUp("Error");
-							audio_id = comp.audio_id;
+								// Update the previous audio ID
+								previous_audio_id = new_audio_id;
+							}
 						}
 					}
 				}
@@ -134,97 +141,101 @@ namespace NIKE {
 				ImGui::Text("Edit Audio Volume & Pitch:");
 
 				//Set volume
+				if (!comp.audio_id.empty() && !comp.channel_group_id.empty())
 				{
-					//Volume before change
-					static float vol_before_change;
+					{
+						//Volume before change
+						static float vol_before_change;
 
-					//Flag for signalling vol changed
-					static bool vol_changed = false;
+						//Flag for signalling vol changed
+						static bool vol_changed = false;
 
-					// Set volume
-					if (ImGui::SliderFloat("Volume", &comp.volume, 0.f, 1.f, "%.2f")) {
-						vol_changed = true;
-					}
-
-					//Check if volume has finished editing
-					if (!ImGui::IsItemActive()) {
-
-						//Check if volume changed
-						if (vol_changed) {
-							LevelEditor::Action change_volume;
-
-							//Change volume do action
-							change_volume.do_action = [&, vol = comp.volume]() {
-								comp.volume = vol;
-								};
-
-							//Change volume undo action
-							change_volume.undo_action = [&, vol = vol_before_change]() {
-								comp.volume = vol;
-								};
-
-							//Execute action
-							NIKE_LVLEDITOR_SERVICE->executeAction(std::move(change_volume));
-
-							vol_changed = false;
+						// Set volume
+						if (ImGui::SliderFloat("Volume", &comp.volume, 0.f, 1.f, "%.2f")) {
+							vol_changed = true;
 						}
 
-						vol_before_change = comp.volume;
+						//Check if volume has finished editing
+						if (!ImGui::IsItemActive()) {
+
+							//Check if volume changed
+							if (vol_changed) {
+								LevelEditor::Action change_volume;
+
+								//Change volume do action
+								change_volume.do_action = [&, vol = comp.volume]() {
+									comp.volume = vol;
+									};
+
+								//Change volume undo action
+								change_volume.undo_action = [&, vol = vol_before_change]() {
+									comp.volume = vol;
+									};
+
+								//Execute action
+								NIKE_LVLEDITOR_SERVICE->executeAction(std::move(change_volume));
+
+								vol_changed = false;
+							}
+
+							vol_before_change = comp.volume;
+						}
 					}
-				}
-
-				//Set pitch
-				{
-					//Pitch before change
-					static float pitch_before_change;
-
-					//Flag for signalling pitch changed
-					static bool pitch_changed = false;
 
 					//Set pitch
-					if (ImGui::SliderFloat("Pitch", &comp.pitch, 0.f, 1.f, "%.2f")) {
-						pitch_changed = true;
-					}
+					{
+						//Pitch before change
+						static float pitch_before_change;
 
-					//Check if pitch has finished editing
-					if (!ImGui::IsItemActive()) {
+						//Flag for signalling pitch changed
+						static bool pitch_changed = false;
 
-						//Check if volume changed
-						if (pitch_changed) {
-							LevelEditor::Action change_pitch;
-
-							//Change pitch do action
-							change_pitch.do_action = [&, pitch = comp.pitch]() {
-								comp.pitch = pitch;
-								};
-
-							//Change [volume]itch undo action
-							change_pitch.undo_action = [&, pitch = pitch_before_change]() {
-								comp.pitch = pitch;
-								};
-
-							//Execute action
-							NIKE_LVLEDITOR_SERVICE->executeAction(std::move(change_pitch));
-
-							pitch_changed = false;
+						//Set pitch
+						if (ImGui::SliderFloat("Pitch", &comp.pitch, 0.f, 1.f, "%.2f")) {
+							pitch_changed = true;
 						}
 
-						pitch_before_change = comp.pitch;
+						//Check if pitch has finished editing
+						if (!ImGui::IsItemActive()) {
+
+							//Check if volume changed
+							if (pitch_changed) {
+								LevelEditor::Action change_pitch;
+
+								//Change pitch do action
+								change_pitch.do_action = [&, pitch = comp.pitch]() {
+									comp.pitch = pitch;
+									};
+
+								//Change [volume]itch undo action
+								change_pitch.undo_action = [&, pitch = pitch_before_change]() {
+									comp.pitch = pitch;
+									};
+
+								//Execute action
+								NIKE_LVLEDITOR_SERVICE->executeAction(std::move(change_pitch));
+
+								pitch_changed = false;
+							}
+
+							pitch_before_change = comp.pitch;
+						}
+					}
+
+					ImGui::Text("Play SFX:");
+
+					// A button to play SFX
+					if (ImGui::ArrowButton("PlayAudio", ImGuiDir_Right)) {
+						if (comp.audio_id != "" && comp.channel_group_id != "") {
+							comp.b_play_sfx = true;
+						}
+						else {
+							comp_panel.setPopUpErrorMsg("Please input valid audio & channel group!");
+							comp_panel.openPopUp("Error");
+						}
 					}
 				}
-
-				ImGui::Text("Play SFX:");
-
-				// A button to play SFX
-				if (ImGui::ArrowButton("PlayAudio", ImGuiDir_Right)) {
-					if (comp.audio_id != "" && comp.channel_group_id != "") {
-						comp.b_play_sfx = true;
-					}
-					else {
-						comp_panel.setPopUpErrorMsg("Please input valid audio & channel group!");
-						comp_panel.openPopUp("Error");
-					}
-				}
+				
 			}
 		);
 	}
