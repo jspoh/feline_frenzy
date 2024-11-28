5c92a2638534aec824bc3b9bd0c7ebdd41c24663
bsohzj <z.soh@digipen.edu>
2024-11-28 12:18:33 +0800

    Registered Shooting comp w sysGameLogic

--- Nikesaurus/src/Components/cShooting.cpp
+++ Nikesaurus/src/Components/cShooting.cpp
@@ -141,6 +141,60 @@ namespace NIKE {
 					}
 				}
 
+				// Static vars for string input mangement
+				static std::string script_input;
+
+				if (ImGui::IsItemActivated() || comp_panel.isEntityChanged()) {
+					script_input = comp.script.script_id;
+				}
+
+
+				// For script path input
+				{
+					ImGui::Text("Enter Your Script:");
+					if (ImGui::InputText("##ScriptInput", script_input.data(), script_input.capacity() + 1)) {
+						script_input.resize(strlen(script_input.c_str()));
+					}
+
+					ImGui::SameLine();
+
+					//Save Button
+					if (ImGui::Button("Save##Script")) {
+						std::filesystem::path script_path = NIKE_LUA_SERVICE->getScriptPath(script_input);
+						if (!script_path.empty() && NIKE_LUA_SERVICE->checkScriptFileExist(script_input))
+						{
+							comp.script.script_path = script_path.string();
+							comp.script.function = NIKE_LUA_SERVICE->extractFunctionFromScript(script_input);
+							LevelEditor::Action save_script_id;
+
+							//Save script id action
+							save_script_id.do_action = [&, script = script_input]() {
+								comp.script.script_id = script;
+								script_input = comp.script.script_id;
+								};
+
+							//Undo save script id action
+							save_script_id.undo_action = [&, script = comp.script.script_id]() {
+								comp.script.script_id = script;
+								script_input = comp.script.script_id;
+								};
+
+							NIKE_LVLEDITOR_SERVICE->executeAction(std::move(save_script_id));
+							comp_panel.setPopUpSuccessMsg("Script Saved successfully");
+							comp_panel.openPopUp("Success");
+
+						}
+						else {
+							comp_panel.setPopUpErrorMsg("Script Does Not Exist!");
+							comp_panel.openPopUp("Error");
+							script_input = comp.script.script_id;
+						}
+					}
+				}
+
+				// Show the extracted function input
+				ImGui::Text("Function: %s", comp.script.function.c_str());
+
 				// !TODO: Change this to input
 				ImGui::Text("Script Path: %s", comp.script.script_path.c_str());
 				ImGui::Text("Function: %s", comp.script.function.c_str());
