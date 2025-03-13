d3e06d986701540f1c655e60279165e70f36f5b0
MintKK <30825268+MintKK@users.noreply.github.com>
2025-03-14 06:48:14 +0800

    Audio transition update + scuffed visual delay

--- Nikesaurus/headers/Managers/Services/sScenes.h
+++ Nikesaurus/headers/Managers/Services/sScenes.h
@@ -1,12 +1,11 @@
 /*****************************************************************//**
  * \file   sScene.h
- * \brief  
- * 
+ * \brief
+ *
  * \author Poh Jing Seng, 2301363, jingseng.poh@digipen.edu (100%)
  * \date   September 2024
- * All content � 2024 DigiPen Institute of Technology Singapore, all rights reserved.
+ * All content © 2024 DigiPen Institute of Technology Singapore, all rights reserved.
  *********************************************************************/
-
 #pragma once
 
 #ifndef M_SCENE_HPP
@@ -15,201 +14,169 @@
 #include "sSerialization.h"
 
 namespace NIKE {
-	namespace Scenes {
+    namespace Scenes {
 
-		//Temporary Disable DLL Export Warning
-		#pragma warning(disable: 4251)
+        //Temporary Disable DLL Export Warning
+#pragma warning(disable: 4251)
 
-		const int MAXLAYERS = 64;
+        const int MAXLAYERS = 64;
 
-		//Forward declaration of Scene service for friending layer
-		class Service;
+        //Forward declaration of Scene service for friending layer
+        class Service;
 
-		//Layer class
-		class NIKE_API Layer {
-		public:
-			using LayerMask = std::bitset<MAXLAYERS>;
-		private:
-			
-			//Friend of layer class
-			friend class Service;
+        //Layer class
+        class NIKE_API Layer {
+        public:
+            using LayerMask = std::bitset<MAXLAYERS>;
+        private:
+            //Friend of layer class
+            friend class Service;
 
-			//Layer mask
-			LayerMask mask;
+            //Layer mask
+            LayerMask mask;
 
-			//Layer mask id
-			unsigned int id;
+            //Layer mask id
+            unsigned int id;
 
-			//Layer state
-			bool b_state;
+            //Layer state
+            bool b_state;
 
-			//Y Sorting on layer
-			bool b_ysort;
+            //Y Sorting on layer
+            bool b_ysort;
 
-			//Vector of entity in order
-			std::vector<Entity::Type> entities;
-		public:
-			Layer() : b_state{ true }, id{ 0 } {}
-			~Layer() = default;
+            //Vector of entity in order
+            std::vector<Entity::Type> entities;
+        public:
+            Layer() : b_state{ true }, id{ 0 } {}
+            ~Layer() = default;
 
-			//Get layer name
-			unsigned int getLayerID() const;
+            //Get layer name
+            unsigned int getLayerID() const;
+            //Set layer state
+            void setLayerState(bool state);
+            //Get layer state
+            bool getLayerState() const;
+            //Set Y Sort state
+            void setLayerYSort(bool y_sort_state);
+            //Get Y Sort state
+            bool getLayerYSort() const;
+            //Set layer mask
+            void setLayerMask(unsigned int mask_id, bool state);
+            //Get layer mask
+            LayerMask getLayerMask() const;
+            //Insert entity
+            void insertEntity(Entity::Type entity);
+            //Remove entity
+            void removeEntity(Entity::Type entity);
+            //Check entity
+            bool checkEntity(Entity::Type entity);
+            //Sort entities
+            void sortEntitiesBasedOnMetaData();
+            //Sort entities Y
+            void sortEntitiesBasedOnYPosition();
+            //Set entity order in layer
+            void setEntityOrder(Entity::Type entity, size_t order_in_layer);
+            //get entity order in layer
+            size_t getEntityOrder(Entity::Type entity) const;
+            //Get entities
+            std::vector<Entity::Type> getEntitites() const;
+            //Get entities size
+            size_t getEntitiesSize() const;
+            //Serialize layer
+            nlohmann::json serialize() const;
+            //Deserialize layer
+            void deserialize(nlohmann::json const& data);
+        };
 
-			//Set layer state
-			void setLayerState(bool state);
+        //Scene manager actions
+        enum class NIKE_API Actions {
+            CHANGE = 0,
+            RESTART,
+            PREVIOUS,
+            RESET,
+            CLOSE
+        };
 
-			//Get layer state
-			bool getLayerState() const;
+        //Change Scene Event
+        struct NIKE_API SceneEvent {
+            Actions scene_action;
+            std::string next_scene_id;
+            //Constructor
+            SceneEvent(Actions scene_action, std::string next_scene_id)
+                : scene_action{ scene_action }, next_scene_id{ next_scene_id } {
+            }
+        };
 
-			//Set Y Sort state
-			void setLayerYSort(bool y_sort_state);
+        // New fade transition members added for audio transition:
+        class NIKE_API Service {
+        private:
+            //Curr scene
+            std::string curr_scene;
+            //Prev scene
+            std::string prev_scene;
+            //Scene event queue
+            std::queue<SceneEvent> event_queue;
+            //Layers within scene
+            std::vector<std::shared_ptr<Layer>> layers;
 
-			//Get Y Sort state
-			bool getLayerYSort() const;
+            // Internal functions for immediate scene change
+            void initScene(std::string const& scene_id);
+            void changeScene(std::string const& scene_id);
+            void restartScene();
+            void previousScene();
 
-			//Set layer mask
-			void setLayerMask(unsigned int mask_id, bool state);
+            // --- New members for fade transition ---
+            bool inTransition = false;
+            float transitionTimer = 0.0f;
+            const float fadeDuration = 1.5f; // seconds for fade-out (and fade-in)
+            enum class FadePhase { None, FadingOut, FadingIn } fadePhase = FadePhase::None;
+            std::string pendingScene;       // scene id to change to
+            float targetBGMVolume = 1.0f;     // store the current global BGM volume at transition start
+            bool restartBGM = true;           // if true, then new scene's BGM track will be restarted
 
-			//Get layer mask
-			LayerMask getLayerMask() const;
+        public:
+            Service() = default;
+            ~Service() = default;
 
-			//Insert entity
-			void insertEntity(Entity::Type entity);
+            //Create layer
+            std::shared_ptr<Layer> createLayer(int index = -1);
+            //Get layer
+            std::shared_ptr<Layer> getLayer(unsigned int layer_id);
+            //Clear layers in curr scene
+            void clearLayers();
+            //Remove layer
+            void removeLayer(unsigned int layer_id);
+            //Check layer
+            bool checkLayer(unsigned int layer_id);
+            //Get layer count
+            unsigned int getLayerCount() const;
+            //Get all layers
+            std::vector<std::shared_ptr<Layer>>& getLayers();
+            //Queue scene event (modified to trigger fade transition when applicable)
+            void queueSceneEvent(SceneEvent&& event);
+            //Set curr scene id
+            void setCurrSceneID(std::string const& new_scene_id);
+            //Get curr scene id
+            std::string getCurrSceneID() const;
+            //Get prev scene id
+            std::string getPrevSceneID() const;
+            // Player data for switching levels
+            static nlohmann::json saved_player_data;
+            static void savePlayerData(nlohmann::json data);
+            static nlohmann::json loadPlayerData();
+            //Restore player data
+            void restorePlayerData();
+            //Reset scene
+            void resetScene();
+            //Init scene service
+            void init();
+            //Update scene events (including processing fade transition)
+            void update();
+        };
 
-			//Remove entity
-			void removeEntity(Entity::Type entity);
+#pragma warning(default: 4251)
 
-			//Check entity
-			bool checkEntity(Entity::Type entity);
-
-			//Sort entities
-			void sortEntitiesBasedOnMetaData();
-
-			//Sort entities Y
-			void sortEntitiesBasedOnYPosition();
-
-			//Set entity order in layer
-			void setEntityOrder(Entity::Type entity, size_t order_in_layer);
-
-			//get entity order in layer
-			size_t getEntityOrder(Entity::Type entity) const;
-
-			//Get entities
-			std::vector<Entity::Type> getEntitites() const;
-
-			//Get entities size
-			size_t getEntitiesSize() const;
-
-			//Serialize layer
-			nlohmann::json serialize() const;
-
-			//Deserialize layer
-			void deserialize(nlohmann::json const& data);
-		};
-
-		//Scene manager actions
-		enum class NIKE_API Actions {
-			CHANGE = 0,
-			RESTART,
-			PREVIOUS,
-			RESET,
-			CLOSE
-		};
-
-		//Change Scene Event
-		struct NIKE_API SceneEvent {
-			Actions scene_action;
-			std::string next_scene_id;
-
-			//Constructor
-			SceneEvent(Actions scene_action, std::string next_scene_id)
-				: scene_action{ scene_action }, next_scene_id{ next_scene_id } {}
-		};
-
-		class NIKE_API Service {
-		private:
-
-			//Curr scene
-			std::string curr_scene;
-
-			//Prev scene
-			std::string prev_scene;
-
-			//Scene event queue
-			std::queue<Scenes::SceneEvent> event_queue;
-
-			//Layers within scene
-			std::vector<std::shared_ptr<Layer>> layers;
-
-			//Init starting scene
-			void initScene(std::string const& scene_id);
-
-			//Change scene
-			void changeScene(std::string const& scene_id);
-
-			//Restart scene
-			void restartScene();
-
-			//Go To Previous scene
-			void previousScene();
-		public:
-			Service() = default;
-			~Service() = default;
-
-			//Create layer
-			std::shared_ptr<Layer> createLayer(int index = -1);
-
-			//Get layer
-			std::shared_ptr<Layer> getLayer(unsigned int layer_id);
-
-			//Clear layers in curr scene
-			void clearLayers();
-
-			//Remove layer
-			void removeLayer(unsigned int layer_id);
-
-			//Check layer
-			bool checkLayer(unsigned int layer_id);
-
-			//Get layer count
-			unsigned int getLayerCount() const;
-
-			//Get all layers
-			std::vector<std::shared_ptr<Layer>>& getLayers();
-
-			//Queue scene event
-			void queueSceneEvent(Scenes::SceneEvent&& event);
-
-			//Set curr scene id
-			void setCurrSceneID(std::string const& new_scene_id);
-
-			//Get curr scene id
-			std::string getCurrSceneID() const;
-
-			//Get prev scene id
-			std::string getPrevSceneID() const;
-
-			// Player data for switching levels
-			static nlohmann::json saved_player_data;
-			static void savePlayerData(nlohmann::json data);
-			static nlohmann::json loadPlayerData();
-
-			//Restore player data
-			void restorePlayerData();
-
-			//Reset scene
-			void resetScene();
-
-			//Init scene service
-			void init();
-
-			//Update scene event
-			void update();
-		};
-
-		//Re-enable DLL Export warning
-		#pragma warning(default: 4251)
-	}
-}
+    } // namespace Scenes
+} // namespace NIKE
 
 #endif // !M_SCENE_HPP
